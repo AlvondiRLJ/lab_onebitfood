cc544c1b8d9c6a47bf73e48e7595585a9ed5f1a0
Alvondi Junior <a.junner@gmail.com>
2019-05-06 17:47:00 -0300

    Added geo, searches and updated controllers

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -15,4 +15,12 @@ class Restaurant < ApplicationRecord
 
   has_one_attached :image
 
+  geocoded_by :address
+
+  after_validation :geocode
+
+  def address
+    [street, number, city, state].compact.join(', ')
+  end
+
 end
